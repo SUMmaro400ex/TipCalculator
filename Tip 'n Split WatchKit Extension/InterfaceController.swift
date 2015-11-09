fee25ce97790626ae7dfda5b85084c40c19d59f6
Jonathan Rose <jonr87ee@gmail.com>
2015-11-08 20:35:34 -0500

    add next function

--- Tip 'n Split WatchKit Extension/InterfaceController.swift
+++ Tip 'n Split WatchKit Extension/InterfaceController.swift
@@ -14,6 +14,10 @@ class InterfaceController: WKInterfaceController {
     
     var labelString:String = "$"
     var tapdNum:String = ""
+    var decimalAdded:Bool = false
+    var numAfterDecimal:Int = 0
+    let green:UIColor = UIColor.greenColor()
+    let numberToSplit:String = "numberToSplit"
     
     @IBOutlet var displayLabel: WKInterfaceLabel!
     @IBAction func tapped0() {tappedNumber(0)}
@@ -26,11 +30,39 @@ class InterfaceController: WKInterfaceController {
     @IBAction func tapped7() {tappedNumber(7)}
     @IBAction func tapped8() {tappedNumber(8)}
     @IBAction func tapped9() {tappedNumber(9)}
+    @IBAction func tappedDecimal() {addDecimal()}
+    @IBAction func nextTapped() {pushControllerWithName(numberToSplit, context: labelString)}
+    @IBOutlet var nextButton: WKInterfaceButton!
     
     func tappedNumber(num:Int){
-        tapdNum = String(num)
-        labelString = labelString.stringByAppendingString(tapdNum)
+        nextButton.setEnabled(true)
+        nextButton.setBackgroundColor(green)
+        if(checkDecimal()){
+            tapdNum = String(num)
+            labelString = labelString.stringByAppendingString(tapdNum)
+            updateDisplay()
+        }
+        return
+    }
+    
+    func checkDecimal()->Bool{
+        if(decimalAdded){
+            numAfterDecimal++
+            if(numAfterDecimal > 2){
+                return false
+            }
+        }
+        return true
+    }
+    
+    func updateDisplay(){
         displayLabel.setText(labelString)
+    }
+    
+    func addDecimal(){
+        decimalAdded = true
+        labelString = labelString.stringByAppendingString(".")
+        updateDisplay()
     }
     
     @IBAction func tappedNext(){
